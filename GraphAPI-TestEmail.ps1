0913118a7dcb4056172ba3484f57083f11bce524
Dave Wilson <dave.wilson@orioneng.com>
2022-10-05 11:34:19 +0100

    Create GraphAPI-TestEmail.ps1
    Test Graph API Email

---
+++ GraphAPI-TestEmail.ps1
@@ -0,0 +1,47 @@
+﻿$clientID = "<client ID Here>"
+$Clientsecret = "<Client Secret Here>"
+$tenantID = "<Tenant ID Here>"
+$MailSender = "<Sender E-Mail Address here>"
+$MailRecipent = "<Target E-Mail Address Here"
+
+#Connect to GRAPH API
+$tokenBody = @{
+    Grant_Type    = "client_credentials"
+    Scope         = "https://graph.microsoft.com/.default"
+    Client_Id     = $clientId
+    Client_Secret = $clientSecret
+}
+$tokenResponse = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$tenantID/oauth2/v2.0/token" -Method POST -Body $tokenBody
+$headers = @{
+    "Authorization" = "Bearer $($tokenResponse.access_token)"
+    "Content-type"  = "application/json"
+}
+
+#Send Mail    
+$URLsend = "https://graph.microsoft.com/v1.0/users/$MailSender/sendMail"
+$BodyJsonsend = @"
+                    {
+                        "message": {
+                          "subject": "Hello World from Microsoft Graph API E-Mail test",
+                          "body": {
+                            "contentType": "HTML",
+                            "content": "This Mail is sent via Microsoft <br>
+                            GRAPH <br>
+                            API<br>
+                            
+                            
+                            "
+                          },
+                          "toRecipients": [
+                            {
+                              "emailAddress": {
+                                "address": "$($MailRecipent)"
+                              }
+                            }
+                          ]
+                        },
+                        "saveToSentItems": "false"
+                      }
+"@
+
+Invoke-RestMethod -Method POST -Uri $URLsend -Headers $headers -Body $BodyJsonsend
